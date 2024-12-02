cbda886fae0d8ce529ed9c54516845717e987bcf
aryyyn <aryan.211618@ncit.edu.np>
2024-12-02 15:22:10 +0545

    fixed a minor bug, and updated the chat code

--- ChatWave/Chat/urls.py
+++ ChatWave/Chat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('chat/', chatView, name='chatViewLogic'),
+    path('chat/<str:chatroom>/', chatView, name='chatViewLogic'),
 
 ]
--- ChatWave/Chat/views.py
+++ ChatWave/Chat/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect,HttpResponse
 from .models import *
 from .forms import *
 
-def chatView(request):
-    chat_room = get_object_or_404(ChatRoom, room_name='Chat_Room_1')
+def chatView(request, chatroom):
+    chat_room = get_object_or_404(ChatRoom, room_name=chatroom)
     chat_messages = chat_room.chat_messages.all().order_by('created')[:50]
     
     context = {
